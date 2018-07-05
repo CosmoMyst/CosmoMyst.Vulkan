f31c66a6595fb85d76d36ddd1959cc60f157075b
CodeMyst <codemyst@pop-os.localdomain>
2018-07-05 20:08:47 +0200

    Project structure

--- MonoMyst.Vulkan/Utilities/VulkanConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MonoMyst.Vulkan.Utilities
-{
-    public class VulkanConstants
-    {
-        public const string VK_STANDARD_VALIDATION = "VK_LAYER_LUNARG_standard_validation";
-        public const string VK_DEBUG_REPORT = "VK_EXT_debug_report";
-        public const string VK_SWAPCHAIN_EXTENSION_NAME = "VK_KHR_swapchain";
-    }
-}
---
+++ source/vulkan.d
@@ -0,0 +1,23 @@
+module monomyst.vulkan;
+
+public void run ()
+{
+    initVulkan ();
+    mainLoop ();
+    cleanup ();
+}
+
+void initVulkan ()
+{
+
+}
+
+void mainLoop ()
+{
+
+}
+
+void cleanup ()
+{
+    
+}
