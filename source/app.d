dad4be0812d25ea7a0addb81c78dd03205a35a0c
CodeMyst <codemyst@outlook.com>
2018-07-05 20:28:40 +0200

    Glfw3 blank window

--- source/app.d
+++ source/app.d
@@ -1,13 +1,6 @@
-import derelict.vulkan;
-import derelict.glfw3.glfw3;
-import std.file : getcwd;
-import std.string : format;
 import monomyst.vulkan;
 
-void main()
+void main ()
 {
-	DerelictGLFW3.load(format("%s/libs/libglfw.so.3.2", getcwd));
-	DerelictVulkan.load(format("%s/libs/libvulkan.so.1", getcwd));
-
 	run ();
 }
--- source/vulkan.d
+++ source/vulkan.d
@@ -1,23 +1,45 @@
 module monomyst.vulkan;
 
+import derelict.vulkan;
+import derelict.glfw3.glfw3;
+import std.file : getcwd;
+import std.string : format;
+
+GLFWwindow* window;
+
 public void run ()
 {
+    initWindow ();
     initVulkan ();
     mainLoop ();
     cleanup ();
 }
 
+void initWindow ()
+{
+	DerelictGLFW3.load (format ("%s/libs/libglfw.so.3.2", getcwd));
+
+    glfwInit ();
+
+    glfwWindowHint (GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint (GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow (800, 600, "MonoMyst.Vulkan", null, null);
+}
+
 void initVulkan ()
 {
-
+	DerelictVulkan.load (format ("%s/libs/libvulkan.so.1", getcwd));
 }
 
 void mainLoop ()
 {
-
+    while (!glfwWindowShouldClose (window))
+        glfwPollEvents ();
 }
 
 void cleanup ()
 {
-    
+    glfwDestroyWindow (window);
+    glfwTerminate ();
 }
