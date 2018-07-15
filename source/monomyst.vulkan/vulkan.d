459b4e7491f2d6a18647cc0a2cff52546f3e2fc7
CodeMyst <codemyst@outlook.com>
2018-07-15 19:26:52 +0200

    Creating the render pass

--- source/monomyst.vulkan/vulkan.d
+++ source/monomyst.vulkan/vulkan.d
@@ -40,6 +40,8 @@ private VkImage [] swapChainImages;
 private VkImageView [] swapChainImageViews;
 private VkFormat swapChainImageFormat;
 private VkExtent2D swapChainExtent;
+private VkRenderPass renderPass;
+private VkPipelineLayout pipelineLayout;
 
 private VkDebugReportCallbackEXT debugCallback;
 
@@ -106,7 +108,39 @@ private void initVulkan ()
 
     createImageViews ();
 
+    createRenderPass ();
+
     createGraphicsPipeline ();
+}
+
+private void createRenderPass ()
+{
+    VkAttachmentDescription colorAttachment = {};
+    colorAttachment.format = swapChainImageFormat;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachmentRef = {};
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentRef;
+
+    VkRenderPassCreateInfo renderPassInfo = {};
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    vkCreateRenderPass (device, &renderPassInfo, null, &renderPass).enforceVk;
 }
 
 private void createGraphicsPipeline ()
@@ -130,6 +164,68 @@ private void createGraphicsPipeline ()
     fragShaderStageInfo.pName = "main";
 
     VkPipelineShaderStageCreateInfo [] shaderStages = [vertShaderStageInfo, fragShaderStageInfo];
+
+    VkPipelineVertexInputStateCreateInfo  vertexInputInfo = {};
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+    VkViewport viewport = {};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = cast (float) swapChainExtent.width;
+    viewport.height = cast (float) swapChainExtent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D scissor = {};
+    scissor.offset.x = 0;
+    scissor.offset.y = 0;
+    scissor.extent = swapChainExtent;
+
+    VkPipelineViewportStateCreateInfo viewportState = {};
+    viewportState.viewportCount = 1;
+    viewportState.pViewports = &viewport;
+    viewportState.scissorCount = 1;
+    viewportState.pScissors = &scissor;
+
+    VkPipelineRasterizationStateCreateInfo rasterizer = {};
+    rasterizer.depthClampEnable = VK_FALSE;
+    rasterizer.rasterizerDiscardEnable = VK_FALSE;
+    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.depthBiasEnable = VK_FALSE;
+
+    VkPipelineMultisampleStateCreateInfo multisampling = {};
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    VkPipelineColorBlendAttachmentState colorBlendAttachement = {};
+    colorBlendAttachement.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
+                                           VK_COLOR_COMPONENT_G_BIT |
+                                           VK_COLOR_COMPONENT_B_BIT |
+                                           VK_COLOR_COMPONENT_A_BIT;
+    colorBlendAttachement.blendEnable = VK_FALSE;
+
+    VkPipelineColorBlendStateCreateInfo colorBlending = {};
+    colorBlending.logicOpEnable = VK_FALSE;
+    colorBlending.attachmentCount = 1;
+    colorBlending.pAttachments = &colorBlendAttachement;
+
+    VkDynamicState [] dynamicStates = [VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_LINE_WIDTH];
+
+    VkPipelineDynamicStateCreateInfo dynamicState = {};
+    dynamicState.dynamicStateCount = 2;
+    dynamicState.pDynamicStates = &dynamicStates [0];
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+
+    vkCreatePipelineLayout (device, &pipelineLayoutInfo, null, &pipelineLayout).enforceVk;
 
     vkDestroyShaderModule (device, fragShaderModule, null);
     vkDestroyShaderModule (device, vertShaderModule, null);
@@ -508,6 +604,10 @@ private void mainLoop ()
 
 private void cleanup ()
 {
+    vkDestroyPipelineLayout (device, pipelineLayout, null);
+
+    vkDestroyRenderPass (device, renderPass, null);
+
     foreach (imageView; swapChainImageViews)
         vkDestroyImageView (device, imageView, null);
 
